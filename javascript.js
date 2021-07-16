4680b5708bd29ebdc020c030f9d313b5bee09320
Lucas Balani <75139281+lucasbalani@users.noreply.github.com>
2021-07-16 14:51:24 -0300

    Upload do portfolio ao GitHub

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function avisojs(){
+    window.alert('Yes! I am Node.js Developer!');
+}
+
+function avisoweb(){
+    window.alert('Yes! I am WEB Developer !')
+}
+
+function avisobulma(){
+    window.alert('Yes! I Work with Bulma ')
+}
+
+function avisoboot(){
+    window.alert('Yes! I Work with BootStrap 4')
+}
